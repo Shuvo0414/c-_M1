e460456232bfc57019fd9fa01557d4b9677f1f8e
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2026-08-12 10:05:45 +0600

    practice in c++  how to work ouput for varibale, then new line , space bettewn two variable values

---
+++ output.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    // std::cout << "Hello world";s
+    int a = 10;
+    long long ll = 10000000000;
+
+    float f = 1.40;
+    double dd = 10.40000000;
+
+    char cc = 'S';
+
+    // printf("%d\n", a);// variable print in c.
+    // std::cout << a; // varible print in C++. In c++ there is no formate specifier for print out like the c program and this the update in c++;
+
+    // std::cout << "My favorite number is " << a << "." << std :: endl; // for new line we can use like "\n" or std :: endl. std :: endl is the update version is c++ program.
+    // std:: cout << a << " is my favorite number.";
+
+
+    // multipale variable print out.
+    // std :: cout << a << " " << ll;
+    // std :: cout << dd << std :: endl << cc; 
+    // std::printf("%.4f\n", dd);
+
+    // std:: cout << "Hello\nC++"; // i can also use new line like that.
+
+    
+
+    return 0;
+}
